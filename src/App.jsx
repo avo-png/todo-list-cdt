2acef73532065234067ce013aa2271660bdb714a
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2026-07-21 15:04:58 -0400

    separated code into components, created form, and imported between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import "./App.css";
+import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 
 function App() {
-	const todoList = [
-		{ id: 1, title: "return packages" },
-		{ id: 2, title: "install barrels" },
-		{ id: 3, title: "clean garage" },
-	];
 	return (
 		<>
 			<div>
 				<h1>My Todos</h1>
-				{/* <ul>
-					{todoList.map((todo) => (
-						<li key={todo.id}>{todo.title}</li>
-					))}
-				</ul> */}
 				<TodoList />
+				<TodoForm />
 			</div>
 		</>
 	);
