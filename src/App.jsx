50e154e3a056b7683ddc208d6fc1b21194a17756
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2026-07-23 12:36:28 -0400

    revisions made on lesson 1 assignment

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,19 @@ import "./App.css";
 
 function App() {
 	const todoList = [
-		{ id: 1, title: "return packages" },
-		{ id: 2, title: "install barrels" },
-		{ id: 3, title: "clean garage" },
+		{ id: 1, title: "review resources" },
+		{ id: 2, title: "take notes" },
+		{ id: 3, title: "code out app" },
 	];
 	return (
-		<>
-			<div>
-				<h1>My Todos</h1>
-				<ul>
-					{todoList.map((todo) => (
-						<li key={todo.id}>{todo.title}</li>
-					))}
-				</ul>
-			</div>
-		</>
+		<div>
+			<h1>My Todos</h1>
+			<ul>
+				{todoList.map((todo) => (
+					<li key={todo.id}>{todo.title}</li>
+				))}
+			</ul>
+		</div>
 	);
 }
 
