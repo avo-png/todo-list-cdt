039a3ae10a747502c48211c9303a255933e900e2
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2026-07-30 12:42:23 -0400

    commented out todoList array and added empty array to initial useState

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import TodoList from "./TodoList";
 import { useState } from "react";
 
 const todos = [
-	{ id: 1, title: "review resources" },
-	{ id: 2, title: "take notes" },
-	{ id: 3, title: "code out app" },
+	// { id: 1, title: "review resources" },
+	// { id: 2, title: "take notes" },
+	// { id: 3, title: "code out app" },
 ];
 
 function App() {
-	const [todoList, setTodoList] = useState(todos);
+	const [todoList, setTodoList] = useState([]);
 	return (
 		<>
 			<div>
