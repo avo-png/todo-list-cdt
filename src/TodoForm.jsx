a7a9425a656b37b8d1e77937d22fe8f1b3c3619a
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2026-07-30 13:42:30 -0400

    updated the handle form submission

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,17 +1,37 @@
-function TodoForm() {
+import { useRef } from "react";
+
+function TodoForm({ onAddTodo }) {
+	const inputRef = useRef();
+
+	const handleAddTodo = (event) => {
+		event.preventDefault();
+
+		// Explore the event object (we'll remove this later)
+		console.log("Event object:", event);
+		console.log("Event target:", event.target);
+		console.log("Input value:", event.target.todoTitle.value);
+
+		// .trim prevents whitespace only todos
+		const todoTitle = event.target.todoTitle.value.trim();
+		if (todoTitle && todoTitle !== "") {
+			onAddTodo(todoTitle);
+			event.target.reset();
+			inputRef.current.focus();
+		}
+	};
+
 	return (
-		<form>
+		<form onSubmit={handleAddTodo}>
 			<label htmlFor="todoTitle">Todo</label>
 			<input
+				ref={inputRef}
 				type="text"
 				id="todoTitle"
+				name="todoTitle"
+				placeholder={"Todo text"}
+				required
 			/>
-			<button
-				type="submit"
-				disabled
-			>
-				Add Todo
-			</button>
+			<button type="submit">Add Todo</button>
 		</form>
 	);
 }
